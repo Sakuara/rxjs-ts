0e2a74155f41bca92edf04180628201cabdc3ec8
sakuara <845888325@qq.com>
2021-01-14 20:02:42 +0800

    add mini-css plugin to take split css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: [
@@ -6,43 +7,32 @@ module.exports = {
     './src/operators.ts'
   ],
   devtool: 'inline-source-map', // 开启sourcemap便于映射到出错代码
+  plugins: [new MiniCssExtractPlugin({
+    filename: '[name].[ext]'
+  })],
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/
       }, {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
         test: /\.less$/i,
-        use: [
-          {
-            loader: "style-loader"
-          }, {
-            loader: "css-loader",
-            options: {
-              modules: true // 开启模块化
-            }
-          }, {
-            loader: 'postcss-loader',
-
-          }, {
-            loader: "less-loader"
-          }
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'less-loader']
       }, {
         test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
           },
-        ],
+        }, ],
+      }, {
+        test: /\.(ttf|woff|eot|svg)$/i,
+        use: ['file-loader'],
       },
     ]
   },
@@ -51,6 +41,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/dist'
+    path: path.resolve(__dirname,'dist'),
+    publicPath: '/'
   }
 };
