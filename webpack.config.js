2eb50915abc27c430feb116ea741ae4d72c142e4
sakuara <kavi.wu@aliyun.com>
2021-01-22 11:28:18 +0800

    config webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,16 @@ module.exports = {
   mode: 'development',
   entry: './src/index.ts', // single entry config
   devtool: 'inline-source-map', // 开启sourcemap便于映射到出错代码
+  devServer: {
+    contentBase: './dist',
+    open: true
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'myApp',
-      template: './src/index.html',
-      filename: 'app.html'
+      //template: './src/index.html', 这两个在使用dev-server时直接注释掉，否则挂载不到server上
+      //filename: 'app.html'
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
